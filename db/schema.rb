6538435e25171b3bc16c6872226dfde4e8cae343
Andy Lim <alim@nspartners.com>
2024-09-13 18:28:03 +0000

    Git migration update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_021220) do
+ActiveRecord::Schema[7.2].define(version: 2024_05_31_021220) do
   create_table "messages", force: :cascade do |t|
     t.integer "room_id", null: false
     t.text "content"
